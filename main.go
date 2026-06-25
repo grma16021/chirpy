f36cd4f571bf5fb1c8056a63c742ce57f2fb3efc
grma16021 <groth.hansen.mathias@gmail.com>
2026-06-25 16:49:17 +0200

    added handlers

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", test)
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
+func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+
+	var cfg = &apiConfig{}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middleWareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", handlerHealth)
+	mux.HandleFunc("/metrics", cfg.handlercount)
+	mux.HandleFunc("/reset", cfg.handlerResetcount)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -22,8 +40,16 @@ func main() {
 
 }
 
-func test(res http.ResponseWriter, req *http.Request) {
+func handlerHealth(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(http.StatusOK)
 	res.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlercount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Load())
+}
+
+func (cfg *apiConfig) handlerResetcount(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
 }
